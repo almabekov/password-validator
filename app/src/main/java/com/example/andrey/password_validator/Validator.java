f29e7076d4b88ebcaeb75b01a1075ca124ed19c4
Unknown <almabekov@gmail.com>
2018-06-05 12:02:39 -0300

    3 more tests added. One test is valid now.

--- app/src/main/java/com/example/andrey/password_validator/Validator.java
+++ app/src/main/java/com/example/andrey/password_validator/Validator.java
@@ -9,6 +9,19 @@ public class Validator {
 
         if (password.length()<8) return false;
         if (password.equals("password")) return false;
+
+        boolean hasLowercase=false;
+        boolean hasDigit=false;
+        boolean hasUppercase=false;
+        boolean hasSpecialCharacter=false;
+
+        //has digit
+        for (int i=0;i<password.length();i++) {
+            if (password.charAt(i)-'0'>=0 && password.charAt(i)-'0'<=9) hasDigit=true;
+
+        }
+        if (hasDigit==false || hasUppercase==false) return false;
+
         return true;
     }
 }
