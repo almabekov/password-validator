be1ca54306fe1b78ef5cd14b102c58640c4810c6
Unknown <almabekov@gmail.com>
2018-06-05 15:34:38 -0300

    added some comments for the code clarity

--- app/src/main/java/com/example/andrey/password_validator/Validator.java
+++ app/src/main/java/com/example/andrey/password_validator/Validator.java
@@ -19,14 +19,16 @@ public class Validator {
 
         //has digit
         for (int i=0;i<password.length();i++) {
+            //if we have digits
             if (Character.isDigit(password.charAt(i))) hasDigit=true;
+            //if we have characters that are not digits, not letters and not whitespaces, it means that we have special characters
             if (!Character.isDigit(password.charAt(i)) && !Character.isLetter(password.charAt(i)) && !Character.isWhitespace(password.charAt(i))) hasSpecialCharacter=true;
         }
         if (hasDigit==false || hasSpecialCharacter==false) return false;
 
         //has lowercase and uppercase
-        if (password.toUpperCase()!=password)  hasLowercase=true;
-        if (password.toLowerCase()!=password) hasUppercase=true;
+        if (password.toUpperCase()!=password)  hasLowercase=true; //if there is a difference, then we had lowercase characters in the original passowrd
+        if (password.toLowerCase()!=password) hasUppercase=true;  //if there is a difference, then we had uppercase characters in the original password
         if (hasLowercase==false || hasUppercase==false) return false;
 
         return true;
