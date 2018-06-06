0c890d4b7f2cdbf6bd750f9d3464246875ea2b98
Unknown <almabekov@gmail.com>
2018-06-06 11:28:07 -0300

    Implemented onclick to validate password.
    Now espresso tests are passed.

--- app/src/main/java/com/example/andrey/password_validator/MainActivity.java
+++ app/src/main/java/com/example/andrey/password_validator/MainActivity.java
@@ -2,12 +2,32 @@ package com.example.andrey.password_validator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button validateButton;
+    EditText editPassword;
+    TextView resultView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        validateButton = findViewById(R.id.valdateButton);
+        editPassword = findViewById(R.id.passwordField);
+        resultView = findViewById(R.id.messageView);
+
+        validateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String tryPassword = editPassword.getText().toString();
+                if (Validator.validate(tryPassword)) resultView.setText("strong password");
+                else resultView.setText("weak password");
+            }
+        });
     }
 }
